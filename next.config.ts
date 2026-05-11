6dfdd36d1734e84b131a4882e75b6c997bc47198
mat <matvei.cherntsov@gmail.com>
2026-05-11 19:03:28 +0400

    feat: allow img path

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.up.railway.app',
+      },
+    ],
   },
 };
 
